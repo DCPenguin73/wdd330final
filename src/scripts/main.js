30aadddf3451095d382d95eccedeb2fabe642f94
echris10sen <chr15016@byui.edu>
2023-11-27 16:25:32 -0700

    Restructured eintire project to be in src folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
-import './app.css'
-import App from './App.svelte'
+import '../styles/app.css'
+import App from '../lib/App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
