c2ea4ffbeda0d8c35e0ab910216f902ae66e5df2
Josh Skouson <Jmatt6836@gmail.com>
2023-11-28 11:13:40 -0700

    Saved us from being murderd by Spotify legal team

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -3,21 +3,31 @@
   import viteLogo from "../assets/vite.svg";
   import Counter from "./Counter.svelte";
   import Header from "./Header.svelte";
+  let src = './images/Spotify_Logo_RGB_White.png'
 </script>
 
 <nav>
   <Header />
 </nav>
 <main>
+  <h1>Spotify Web Player</h1>
+  <h2>Powered but not endorsed by Spotify</h2>
   <section>
     <a href="#"><button>Create New Radio</button></a>
     <a href="#"><button>Manage Radios</button></a>
     <a href="#"><button>Search</button></a>
   </section>
+  <img {src} alt="The Spotify logo" />
 </main>
 
 <style>
   nav {
     max-width: fit-content;
+   
+  }
+  img{
+    max-width:150px;
+    padding:15px;
+    float:right;
   }
 </style>
