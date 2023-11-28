e87385872e685704add2bfc4b0dc314909dcf50f
echris10sen <chr15016@byui.edu>
2023-11-28 09:52:59 -0700

    Added a hamburger menu to the home page: Eric

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -2,48 +2,22 @@
   import svelteLogo from "../assets/svelte.svg";
   import viteLogo from "../assets/vite.svg";
   import Counter from "./Counter.svelte";
+  import Header from "./Header.svelte";
 </script>
 
+<nav>
+  <Header />
+</nav>
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <section>
+    <a href="#"><button>Create New Radio</button></a>
+    <a href="#"><button>Manage Radios</button></a>
+    <a href="#"><button>Search</button></a>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  nav {
+    max-width: fit-content;
   }
 </style>
