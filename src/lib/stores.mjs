9fb3120371958fcf28c0eaa510694ceb9250a793
Daniel Carr <DBCarr980@gmail.com>
2023-12-09 17:15:29 -0700

    Switching Laptops

--- src/lib/stores.mjs
+++ src/lib/stores.mjs
@@ -5,8 +5,11 @@ export const route = writable("#home");
 export let userStore = writable({ isLoggedIn: false});
 
 export function loggedIn(){
+    console.log(`In logged in`);
     const check = localStorage.getItem("code_verifier")
+    console.log(check);
     if(check != null){
+        console.log(`Hello`);
         userStore = writable({isLoggedIn: true});
     }
 }
