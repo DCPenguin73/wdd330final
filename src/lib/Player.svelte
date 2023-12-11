c52e0a0373e577dc5fb2c372930bc94c27f436b7
Daniel Carr <DBCarr980@gmail.com>
2023-12-11 10:59:25 -0700

    Update Player.svelte

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -14,12 +14,11 @@
     };
   
     let isPaused = false;
-    $: isActive = false;
+    let isActive = false;
     let player;
     let currentTrack = track;
   
     onMount(() => {
-     
       const script = document.createElement("script");
       script.src = "https://sdk.scdn.co/spotify-player.js";
       script.async = true;
@@ -27,22 +26,21 @@
       document.body.appendChild(script);
   
       window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = window.localStorage.getItem("access_token");
-        const player = new Spotify.Player({
+        player = new window.Spotify.Player({
           name: 'Web Playback SDK',
-          getOAuthToken: cb => { cb(token); },
+          getOAuthToken: cb => { cb(props.token); },
           volume: 0.5
         });
+  
         player.addListener('ready', ({ device_id }) => {
           console.log('Ready with Device ID', device_id);
         });
-        
+  
         player.addListener('not_ready', ({ device_id }) => {
           console.log('Device ID has gone offline', device_id);
         });
-        
+  
         player.addListener('player_state_changed', (state) => {
-          
           if (!state) {
             return;
           }
@@ -52,7 +50,6 @@
   
           player.getCurrentState().then(state => {
             (!state) ? isActive = false : isActive = true;
-            console.log(state);
           });
         });
   
